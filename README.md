8d3600e335f6c95c1bf0e627a85b631a596e6504
frostblooded <frostblooded@yahoo.com>
2018-05-05 23:00:27 +0300

    Make input a little more user-friendly

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# word_snack_bruteforcer
-A ruby script (maybe a full web app one day) that tells you all words with the given letters that are actual words in the Bulgarian dictionary (provided by Wiktionary).
+# Word Snack Bruteforcer
+A ruby script (maybe a full web app one day) that tells you all words with the given letters that are actual words in the Bulgarian dictionary.
--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
 
-puts 'Enter letters seperated by commas (example: "а,б,ж,в,д"):'
-letters = gets.chomp.split(',')
-puts 'Enter what lengths do you want separated by commas' \
-     '(example: "3,4,5" to find three words with length 3, 4 and 5 respectively'
-wanted_lengths = gets.chomp.split(',').map(&:to_i)
+puts 'Enter letters (example: "абжвд"):'
+letters = gets.chomp.split ''
+puts 'Enter what lengths separated by a single space'\
+     ' (example: "3 4 5" to find words with length 3, 4 and 5 respectively'
+lengths = gets.chomp.split(' ').map(&:to_i)
 
-wanted_lengths.each do |length|
+puts letters
+puts lengths
+
+lengths.each do |length|
   permutations = letters.permutation(length).to_a
   words = permutations.map(&:join)
+  puts words
 end
