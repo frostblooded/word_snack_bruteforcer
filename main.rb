9118b47cfdbe74df16dde826e34fa5077b873511
frostblooded <frostblooded@yahoo.com>
2018-05-08 10:43:53 +0300

    Make it actually work, but is very slow

--- main.rb
+++ main.rb
@@ -1,17 +1,13 @@
 def real_word?(word)
-  words = File.readlines 'words.txt'
-  res = words.bsearch {|line| line <=> word}
-  res != nil
+  words = File.readlines('words.txt').map(&:chomp)
+  words.include? word
 end
 
-# puts 'Enter letters (example: "абжвд"):'
-# letters = gets.chomp.split ''
-# puts 'Enter what lengths separated by a single space'\
-#      ' (example: "3 4 5" to find words with length 3, 4 and 5 respectively'
-# lengths = gets.chomp.split(' ').map(&:to_i)
-
-letters = %w[г о в р е т а]
-lengths = [5, 6]
+puts 'Enter letters (example: "абжвд"):'
+letters = gets.chomp.split ''
+puts 'Enter what lengths separated by a single space'\
+     ' (example: "3 4 5" to find words with length 3, 4 and 5 respectively'
+lengths = gets.chomp.split(' ').map(&:to_i)
 
 lengths.each do |length|
   permutations = letters.permutation(length).to_a
