e57525986d97b48f436ad3110be46596867a8d35
frostblooded <frostblooded@yahoo.com>
2018-05-08 10:07:40 +0300

    Binary search doesn't work, but made dictionary all lowercase

--- main.rb
+++ main.rb
@@ -1,15 +1,23 @@
+def real_word?(word)
+  words = File.readlines 'words.txt'
+  res = words.bsearch {|line| line <=> word}
+  res != nil
+end
 
-puts 'Enter letters (example: "абжвд"):'
-letters = gets.chomp.split ''
-puts 'Enter what lengths separated by a single space'\
-     ' (example: "3 4 5" to find words with length 3, 4 and 5 respectively'
-lengths = gets.chomp.split(' ').map(&:to_i)
+# puts 'Enter letters (example: "абжвд"):'
+# letters = gets.chomp.split ''
+# puts 'Enter what lengths separated by a single space'\
+#      ' (example: "3 4 5" to find words with length 3, 4 and 5 respectively'
+# lengths = gets.chomp.split(' ').map(&:to_i)
 
-puts letters
-puts lengths
+letters = %w[г о в р е т а]
+lengths = [5, 6]
 
 lengths.each do |length|
   permutations = letters.permutation(length).to_a
   words = permutations.map(&:join)
 
+  words.each do |word|
+    puts word if real_word? word
+  end
 end
